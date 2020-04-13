65e845934d06543f532def1567fd9b470fa1f344
Raven Hon <hon@codeastar.com>
2020-04-13 21:35:09 +0800

    Update getairportcode.py

--- getairportcode.py
+++ getairportcode.py
@@ -10,7 +10,6 @@ Airports = db.table('Airports')
 ENDPOINT_PREFIX = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/"
 MARKET = "US"
 CURRENCY = "USD"
-COUNTRY_ENDPOINT = "http://country.io/names.json"
 SLEEP_BUFFER = 5
 
 def initProfileDB():
@@ -32,28 +31,31 @@ def handleAPIException(responseText, apiname):
       print(json.dumps(json.loads(responseText), indent=3, sort_keys=True))
       sys.exit(f"API exception on [{apiname}]")    
 
-def getCountries():
+def getCountries(headers):
     country_list = []
     ss_countries = Countries.search(Query())
+    request_start_time=0
     if len(ss_countries) == 0:
-        response = requests.request("GET", COUNTRY_ENDPOINT)
+        request_start_time = time.time()
+        url = ENDPOINT_PREFIX+f"reference/v1.0/countries/en-US"
+        response = requests.request("GET", url, headers=headers)
         if response.status_code != 200: handleAPIException(response.text, "getCountries")
-        country_json = json.loads(response.text)
-        Countries.insert(country_json)
-        ss_countries = [country_json]
-    for country in ss_countries[0]:
-        country_list.append(ss_countries[0][country])
-    return country_list
+        ss_countries = json.loads(response.text)
+        Countries.insert_multiple(ss_countries["Countries"])
+        ss_countries = ss_countries["Countries"]
+    for country in ss_countries:
+        country_list.append(country['Name'])    
+    return country_list, request_start_time    
 
 def getIataCode(place_string, request_time_list):
     url = ENDPOINT_PREFIX+f"autosuggest/v1.0/{MARKET}/{CURRENCY}/en-US/"
     querystring = {"query":place_string}
     request_start_time = time.time()
     request_time_list.append(request_start_time)
-    if (len(request_time_list) % 50 == 0): 
+    if (len(request_time_list) % 40 == 0): 
         the_first_request_time = request_time_list[0]
         second_from_1st_request = round(time.time() - the_first_request_time)
-        print(f"Hit the 50th request mark, wait for 60-{second_from_1st_request} seconds")
+        print(f"Hit the 40th request mark, wait for 60-{second_from_1st_request} seconds")
         time.sleep(60-second_from_1st_request+SLEEP_BUFFER)
         request_time_list =[]
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -79,8 +81,9 @@ headers = {
 airports = Airports.search(Query().Iata)
 if airports == []: 
     print("No Airport found, start requesting from Skyscanner API...")
-    country_list = getCountries()
     request_time_list = []
+    country_list, request_start_time = getCountries(headers)
+    if (request_start_time > 0): request_time_list.append(request_start_time)
     for country in country_list:
         request_time_list = getIataCode(country, request_time_list)
 
